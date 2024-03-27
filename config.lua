ea12f9a544ae3cd7de396b1cbe6586b849c55ae0
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-03-27 20:38:31 +0100

    fix: better intervals in config

--- config.lua
+++ config.lua
@@ -1,13 +1,15 @@
 Config = {}
 
 Config.commandGroups = { "group.mod", "group.admin" }
-Config.interval = 61000 -- 10 seconds
+Config.interval = 30 * 60 * 1000 -- 30 minutes
 Config.notifies = {
-	1000,
-	2000,
-	3000,
-	5000,
-	10000,
-	30000,
-	60000,
+	10 * 60 * 1000,                -- 10 minutes,
+	5 * 60 * 1000,                 -- 5 minutes,
+	3 * 60 * 1000,                 -- 3 minute,
+	1 * 60 * 1000,                 -- 1 minute,
+	30 * 1000,                     -- 30 seconds,
+	10 * 1000,                     -- 10 seconds,
+	3 * 1000,                      -- 3 seconds,
+	2 * 1000,                      -- 2 second,
+	1 * 1000,                      -- 1 second
 }
